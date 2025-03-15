355495abf98c738923acd228539a9e73d5bfdd7b
Mahak <mahakhardiya@gmail.com>
2025-03-15 12:08:47 +0530

    oops

---
+++ Java Assignment/Arrays/Average.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n]; 
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i]; 
+        }
+
+        double average = (double) sum / n;
+
+        System.out.println("The average of the array elements is: " + average);
+
+        sc.close();
+    }
+}
+
